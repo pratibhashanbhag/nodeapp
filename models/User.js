bf317f525d0599b37632e9dd99f1a53ac825a520
pratibhashanbhag <167669485+pratibhashanbhag@users.noreply.github.com>
2025-12-09 16:17:17 +0530

    Add files via upload

---
+++ models/User.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/^\S+@\S+\.\S+$/, "Please enter a valid email address"],
+    },
+
+
+    password:{
+         type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true, // Adds createdAt and updatedAt fields
+  }
+);
+
+
+const User = mongoose.model("User", userSchema);
+
+
+module.exports = User;
+
---
+++ routes/users.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const {
+  createUser,
+  getAllUsers,
+  getUserById,
+  updateUser,
+  deleteUser,
+  patchUpdateUser
+} = require("../controllers/userController");
+
+
+const router = express.Router();
+
+
+// POST /api/users - Create a new user
+router.post("/", createUser);
+router.get("/", getAllUsers);
+router.get("/:id", getUserById);
+router.put("/:id", updateUser);
+router.delete("/:id", deleteUser);
+router.put("/:id",patchUpdateUser)
+module.exports = router;
